9f3ec850d51c2922a516b708bea9cc24f43cdf97
Sakaki-Aruka <99568054+Sakaki-Aruka@users.noreply.github.com>
2022-09-05 16:43:26 +0900

    update : .gitignore

---
+++ src/main/java/chunkgenerator/chunkgenerator/ChunkGenerator.java
@@ -0,0 +1,17 @@
+package chunkgenerator.chunkgenerator;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ChunkGenerator extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
